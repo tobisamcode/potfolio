49248bff32bcd1d852b098785cca8353723d7790
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-08-02 01:13:23 +0100

    added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+Function.prototype.extend = function(fn) {
+    var self = this;
+    return function() {
+        self.apply(this, arguments);
+        fn.apply(this, arguments);
+    };
+};
+
+// Homepage Typed.js
 const introText = "TOBI SAMUEL"
 const introText2 = "A Frontend Web Developer."
 
@@ -24,7 +33,34 @@ const writeIntro2 = () => {
     }
 }
 
+// About Typed.js
 
+const introAbout = "about me"
+const introAbout2 = "Meticulous web developer with over 2 years of front end experience and passion for responsive website design and a firm believer in the mobile-first approach. W3C certified. Implemented new responsive website approach which increased mobile traffic by 20%. ."
+
+let m = 0;
+let n = 0
+const fast = 80;
+
+const writeAbout1 = () => {
+    if (m < introAbout.length) {
+        document.querySelector("#about1").innerHTML += introAbout.charAt(m);
+        m++
+        setTimeout(writeAbout1, fast)
+    } else {
+        setTimeout(writeAbout2, 60)
+    }
+}
+
+
+const writeAbout2 = () => {
+    if (n < introAbout2.length) {
+        document.querySelector("#about2").innerHTML += introAbout2.charAt(n);
+        n++
+        setTimeout(writeAbout2, fast)
+    }
+}
 window.onload = () => {
     writeIntro1()
 }
+window.onload = window.onload.extend(writeAbout1())
